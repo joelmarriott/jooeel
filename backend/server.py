4c6135dca7b4f4b7de5db9f63f65cf0aa660bed6
Joel Marriott <joel.marriott.uk@gmail.com>
2023-01-21 21:58:17 +0000

    Yet more fixes

--- backend/server.py
+++ backend/server.py
@@ -3,29 +3,33 @@ from flask_socketio import SocketIO, emit
 
 
 app = Flask(__name__)
-socket = SocketIO(app, cors_allowed_origins="*")
+socket = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
+
+
 @app.route('/')
-
-
 def index():
-    return render_template('index.html')@socket.on('connect')
+    return render_template('index.html')
 
 
+@socket.on('connect')
 def connect():
-    print("[CLIENT CONNECTED]:", request.sid)@socket.on('disconnect')
+    print("[CLIENT CONNECTED]:", request.sid)
 
-
+    
+@socket.on('disconnect')
 def disconn():
-    print("[CLIENT DISCONNECTED]:", request.sid)@socket.on('notify')
+    print("[CLIENT DISCONNECTED]:", request.sid)
 
 
+@socket.on('notify')
 def notify(user):
-    emit('notify', user, broadcast=True, skip_sid=request.sid)@socket.on('data')
+    emit('notify', user, broadcast=True, skip_sid=request.sid)
 
-
+    
+@socket.on('data')
 def emitback(data):
     emit('returndata', data, broadcast=True)
 
     
 if __name__ == "__main__":
-    socket.run(app)
+    socket.run(app, "0.0.0.0", "5000")
